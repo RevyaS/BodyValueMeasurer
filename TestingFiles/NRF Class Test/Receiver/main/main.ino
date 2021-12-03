475898f68f5abded90508696fd2785674c69316f
RevyaS <84831377+RevyaS@users.noreply.github.com>
2021-12-03 21:06:16 +0800

    Add files via upload

---
+++ TestingFiles/NRF Class Test/Receiver/main/main.ino
@@ -0,0 +1,18 @@
+#include "Receiver.h"
+
+uint64_t address = 250;
+Receiver receiver(address);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(receiver.isAvailable())
+  {
+    receiver.receive();
+    Serial.println(receiver.getSerialString());
+  } else {
+    Serial.println("Radio Unavailable");
+  }
+}
