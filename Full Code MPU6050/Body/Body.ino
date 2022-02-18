ed0f2ae27f560e9058c70053eef086e7406b3026
RevyaS <84831377+RevyaS@users.noreply.github.com>
2022-02-18 13:59:26 +0800

    Add files via upload

---
+++ Full Code MPU6050/Body/Body.ino
@@ -0,0 +1,23 @@
+#include "Reader.h"
+
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "SPI.h"
+
+//Arbitrary Address that should be shared by Receiver & Transmitter
+const uint64_t address = 250;
+
+Reader reader;
+RF24 radio(9,10);
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.stopListening();
+}
+
+void loop() {
+  float values = reader.readValues();
+  radio.write(&values, sizeof(values));
+  delay(300);
+}
